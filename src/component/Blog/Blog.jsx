e2c4034dfb97228fc8027394e0b3c2453d3e2081
JabirSiddiqueTalim <jsiddiquetalim@gmail.com>
2026-03-09 17:00:53 +0600

    added bookmark

--- src/component/Blog/Blog.jsx
+++ src/component/Blog/Blog.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
+import './Blog.css';
+import { FaBookmark } from "react-icons/fa";
 
 const Blog = ({ blog }) => {
   return (
-    <div>
-      <h2>Single Blog :{blog.id} </h2>
+    <div className='card'>
+      
       <div className="card bg-base-100 w-96 shadow-sm">
+      <h2 className='text-xl'>Blog id :{blog.id} </h2>
         <figure>
-          <img
+       
+          <img className='BlogImage'
             src={blog.image}
             alt="Shoes" />
+           
         </figure>
         <div className="card-body">
           <h2 className="card-title">
+            
             {blog.name}
             <div className="badge badge-secondary">NEW</div>
-          </h2>
+            < FaBookmark size={20}/>
+          </h2> 
           <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
           <div className="card-actions justify-end">
             <div className="badge badge-outline">Fashion</div>
